ea4ba5e6cb00633e5792c9e4c6efd60e17ce59df
vagrant <vagrant@vagrant.vm>
2017-04-15 23:38:55 +0000

    Finished up through part 4

--- assessment.py
+++ assessment.py
@@ -43,7 +43,7 @@ Part 1: Discussion
    Give an example of when you might use each.
 
    A class attribute is defined in the class definition. When an instance of
-   a class occurs, that instance immediately holds each of those attributes.
+   a class occurs, that instance immediately holds each of the class attributes.
    However, if you explicitly assign an instance its own particular attribute,
    then it has an instance attribute that is unique from other instances of the
    class.
@@ -53,18 +53,24 @@ Part 1: Discussion
                 profession = "student"
                 job = "to learn"
 
-    then ever instance of Student has the class attributes of profession and job,
+    then every instance of Student has the class attributes profession and job,
     and the value is the same for each.
 
-    But say, one of your instances (joe) decides to get a job in the cafeteria.
+    But, say one of your instances (joe) decides to get a job in the cafeteria.
     You would do the following:
 
             joe.job = "serve food"
 
     The instance joe now has a unique instance attribute of job = "serve food",
-    and that attribute supercedes the class attribute of job. When searching for
+    and that attribute supercedes the class attribute job. When searching for
     a value of an instance attribute, Python first searches in the instance
     attributes, and if not there, then moves to search in the class attributes.
+
+    You would assign an instance attribute if there is something unique about that
+    instance that you want to be reflected in your code (because it will be used
+    over the class attribute). On the other hand, if you want the instance to have
+    the same attributes as the rest of its class, you would defer to the class
+    attribute and not assign an instance attribute.
 
 
 """
@@ -72,3 +78,138 @@ Part 1: Discussion
 
 # Parts 2 through 5:
 # Create your classes and class methods
+
+class Student(object):
+    """ A student. """
+
+    def __init__(self, first_name, last_name, address):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        # example in the instructions shows a single dictionary of all attributes
+        self.data = {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'address': self.address
+            }
+
+    def __repr__(self):
+        return "A student with a first name, last name, and address."
+
+
+class Question(object):
+    """ A question and answer. """
+
+    def __init__(self, question, correct_answer):
+        self.question = question
+        self.correct_answer = correct_answer
+        self.data = {
+            'question': self.question,
+            'correct_answer': self.correct_answer
+            }
+
+    def __repr__(self):
+        return "A question and a correct answer."
+
+
+class Exam(object):
+    """ An exam. """
+
+    def __init__(self, name):
+        self.questions = []
+        self.name = name
+        self.data = {
+            'name': self.name,
+            'questions': self.questions
+            }
+
+    def __repr__(self):
+        return "List of dictionaries, each w/ an exam question & correct answer."
+
+    def add_question(self, question):
+        self.questions.append({ "question": question.question,
+                                "correct_answer": question.correct_answer})
+
+    def ask_and_evaluate(self, i=-1):    # defaults to last question added
+        question = self.questions[i]['question']
+        correct_answer = self.questions[i]['correct_answer']
+        user_answer = raw_input("{} > ".format(question))
+        if user_answer == correct_answer:
+            return True
+        else:
+            return False
+
+    def administer(self):
+        questions_asked = 0
+        questions_correct = 0
+
+        for i in range(len(self.questions)):
+            questions_asked += 1
+            if self.ask_and_evaluate(i) is True:
+                questions_correct += 1
+
+        score = 100 * float(questions_correct) / questions_asked
+        print score
+
+
+class Quiz(Exam):
+    """ A quiz. """
+
+    def __repr__(self):
+        return "List of dictionaries, each w/ a quiz question & correct answer."
+
+
+class StudentExam(object):
+    """ An exam taken by a student. """
+
+    def __init__(self, student, exam):
+        self.name = student.first_name
+        self.exam = exam
+        self.score = None
+
+    def __repr__(self):
+        return "A student's name, an exam they took, and their score on the exam"
+
+    def take_test(self):
+        self.exam.administer()
+
+
+def example():
+    """ Creates an exam and a student, then administers the exam to the student. """
+
+    student = Student('Michela', 'Bestwick', '235 Mallorca Way')
+
+    q1 = Question("What is the capitol of California?", "Sacramento")
+    q2 = Question("Who won the 1989 World Series?", "Oakland A's")
+    q3 = Question("What was Ronald Reagan's favorite candy?", "jelly beans")
+
+    midterm = Exam('midterm')
+
+    midterm.add_question(q1)
+    midterm.add_question(q2)
+    midterm.add_question(q3)
+
+    student_exam = StudentExam(student, midterm)
+
+    student_exam.take_test()
+
+
+example()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
